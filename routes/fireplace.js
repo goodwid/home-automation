9d867e4e39bb150b32700a9349f3844404f885e3
David Goodwin <goodwid@gmail.com>
2016-12-06 00:54:55 -0800

    wtf

--- routes/fireplace.js
+++ routes/fireplace.js
@@ -2,18 +2,15 @@ const router = require('express').Router();
 const request = require('superagent');
 const fp = process.env.FIREPLACE_URI;
 const fpAPI = process.env.FIREPLACE_API_KEY;
-console.log('fpAPI: ', fpAPI);
 
 module.exports = router
   .get('/on', (req, res, next) => {
     request
-      .get(`${fp}/on`)
+      .get(fp + '/on')
       .set('Authorization', fpAPI)
       .end((err, response) => {
-        console.log('*** ERR: ', err);
-        if (err) next(err);
-        console.log(response);
-        res.send('response.text');
+        if (err) return next(err);
+        res.send(response.text);
       });
   })
   .get('/off', (req, res, next) => {
@@ -21,7 +18,7 @@ module.exports = router
       .get(`${fp}/off`)
       .set('Authorization', fpAPI)
       .end((err, response) => {
-        if (err) next(err);
+        if (err) return next(err);
         res.send(response.text);
       });
   });
