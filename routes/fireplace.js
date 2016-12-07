706fb4fa0113f7c301d7c7e11ecc4f21cdc4be02
David Goodwin <goodwid@gmail.com>
2016-12-06 18:44:08 -0800

    First working set

--- routes/fireplace.js
+++ routes/fireplace.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bodyParser = require('body-parser').json();
 const request = require('superagent');
 const fp = process.env.FIREPLACE_URI;
 const fpAPI = process.env.FIREPLACE_API_KEY;
@@ -6,7 +7,7 @@ const fpAPI = process.env.FIREPLACE_API_KEY;
 module.exports = router
   .get('/on', (req, res, next) => {
     request
-      .get(fp + '/on')
+      .get(`${fp}/on`)
       .set('Authorization', fpAPI)
       .end((err, response) => {
         if (err) return next(err);
@@ -16,6 +17,25 @@ module.exports = router
   .get('/off', (req, res, next) => {
     request
       .get(`${fp}/off`)
+      .set('Authorization', fpAPI)
+      .end((err, response) => {
+        if (err) return next(err);
+        res.send(response.text);
+      });
+  })
+  .get('/status', (req, res, next) => {
+    request
+      .get(`${fp}/status`)
+      .set('Authorization', fpAPI)
+      .end((err, response) => {
+        if (err) return next(err);
+        res.send(response.text);
+      });
+  })
+  .post('/timer', bodyParser, (req, res, next) => {
+    request
+      .post(`${fp}/timer`)
+      .query({timeout: req.body.timeout})
       .set('Authorization', fpAPI)
       .end((err, response) => {
         if (err) return next(err);
